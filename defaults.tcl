9d59de3e3fcaad097ce2a40dbf091798b157969c
sls <sls>
1995-02-10 11:01:26 +0000

    Remove font defs.

--- defaults.tcl
+++ defaults.tcl
@@ -10,10 +10,6 @@ proc tkinspect_default_options {} {
     option add *Label*borderWidth 0
     option add *Frame.highlightThickness 0
     option add *Frame.borderWidth 2
-    option add *Value*vname*font \
-	-adobe-courier-medium-r-*-*-*-130-*-*-*-*-iso8859-*
-    option add *Text*font \
-	-adobe-courier-medium-r-*-*-*-130-*-*-*-*-iso8859-*
     option add *Tkinspect_main*Menubutton.borderWidth 0
     option add *Procs_list.patterns {
 	^tk[A-Z].*
