64ec58a68525ff589e96fbeded11a5941b3e599d
sls <sls>
1995-02-15 02:57:56 +0000

    Source files at global level.

--- stl.tcl
+++ stl.tcl
@@ -2,13 +2,13 @@
 # $Id$
 #
 # Stl is my own tcl library.  It's not quite ready to be released.
-# In the stl-lite directory is an extremely trimmed down version.
+# The stl-lite directory contains an extremely trimmed down version.
 # This proc loads it.
 #
 
 proc stl_lite_init {} {
     global tkinspect_library
     foreach file {object.tcl filechsr.tcl simpleentry.tcl tk_util.tcl} {
-	source $tkinspect_library/stl-lite/$file
+	uplevel #0 [list source $tkinspect_library/stl-lite/$file]
     }
 }
