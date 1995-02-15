f7722fc470b9bc7fc12e90e4e1baf3ad741881d9
sls <sls>
1995-02-15 02:59:34 +0000

    Initial revision

---
+++ version.tcl
@@ -0,0 +1,11 @@
+#
+# $Id$
+#
+# Contains version numbers for tkinspect-5.
+#
+
+proc version_init {} {
+    global tkinspect
+    set tkinspect(release) 5.0.4
+    set tkinspect(release_date) "Feb 14, 1995"
+}
