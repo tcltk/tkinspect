e1e72ea75ed0a7f86f0f3efcc0d1d9c0497d9fb0
sls <sls>
1995-02-16 00:09:08 +0000

    Add a Tk version check.

--- version.tcl
+++ version.tcl
@@ -5,7 +5,14 @@
 #
 
 proc version_init {} {
-    global tkinspect
-    set tkinspect(release) 5.0.4
+    global tkinspect tk_version
+    set tkinspect(release) 5.0.5
     set tkinspect(release_date) "Feb 14, 1995"
+    scan $tk_version "%d.%d" major minor
+    if {$major != 4} {
+	puts stderr \
+      "tkinspect-5 requires Tk 4.x, you appear to be running Tk $major.$minor"
+	exit 1
+    }
 }
+
