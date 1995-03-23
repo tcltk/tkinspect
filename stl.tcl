1fd9dd0ee0ef85ce300cc529e44af9e7f707ec53
sls <sls>
1995-03-23 06:57:45 +0000

    Add feedback.tcl

--- stl.tcl
+++ stl.tcl
@@ -8,7 +8,9 @@
 
 proc stl_lite_init {} {
     global tkinspect_library
-    foreach file {object.tcl filechsr.tcl simpleentry.tcl tk_util.tcl} {
+    foreach file {
+	object.tcl filechsr.tcl simpleentry.tcl tk_util.tcl feedback.tcl
+    } {
 	uplevel #0 [list source $tkinspect_library/stl-lite/$file]
     }
 }
