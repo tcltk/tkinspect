507dd0d8ce91401ba9fd7f0c0909aa32e2e0e5bb
sls <sls>
1995-06-02 06:52:51 +0000

    Bump to 5.1.0.

--- version.tcl
+++ version.tcl
@@ -6,8 +6,8 @@
 
 proc version_init {} {
     global tkinspect tk_version
-    set tkinspect(release) 5.0.5
-    set tkinspect(release_date) "Feb 14, 1995"
+    set tkinspect(release) 5.1.0
+    set tkinspect(release_date) "June 1, 1995"
     scan $tk_version "%d.%d" major minor
     if {$major != 4} {
 	puts stderr \
