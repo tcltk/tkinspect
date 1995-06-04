ceb24c46a7299ee498dc54698181e647ce77e9e7
sls <sls>
1995-06-04 08:06:54 +0000

    Add tkhtml.tcl.

--- stl.tcl
+++ stl.tcl
@@ -10,6 +10,7 @@ proc stl_lite_init {} {
     global tkinspect_library
     foreach file {
 	object.tcl filechsr.tcl simpleentry.tcl tk_util.tcl feedback.tcl
+	tkhtml.tcl
     } {
 	uplevel #0 [list source $tkinspect_library/stl-lite/$file]
     }
