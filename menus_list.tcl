151b99ff2fa0d77d10f81c48e64fc65fef8b6106
sls <sls>
1995-06-21 21:06:01 +0000

    Add update_self method.

--- menus_list.tcl
+++ menus_list.tcl
@@ -1,11 +1,17 @@
 #
 # $Id$
 #
+# Handles browsing menus.
+#
 
 widget menus_list {
     object_include tkinspect_list
     param title "Menus"
     method get_item_name {} { return menu }
+    method update_self {target} {
+	$slot(main) windows_info update $target
+	$self update $target
+    }
     method update {target} {
 	$self clear
 	foreach w [$slot(main) windows_info get_windows] {
